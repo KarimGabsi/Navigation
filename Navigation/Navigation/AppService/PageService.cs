8194001e7472d5da788ea23f36aa9209b0ce87bf
gabsi <karim.gabsi@vives.be>
2021-11-22 16:37:51 +0100

    page service implementation

--- Navigation/Navigation/AppService/PageService.cs
+++ Navigation/Navigation/AppService/PageService.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace Navigation.AppService
 {
-    public class PageService
+    public class PageService : IPageService
     {
+        public async Task DisplayAlert(string title, string message, string cancel)
+        {
+            await Application.Current.MainPage.DisplayAlert(title, message, cancel);
+        }
 
+        public async Task<bool> DisplayAlert(string title, string message, string accept, string cancel)
+        {
+            return await Application.Current.MainPage.DisplayAlert(title, message, accept, cancel);
+        }
+
+        public async Task PushAync(Page page)
+        {
+            await Application.Current.MainPage.Navigation.PushAsync(page);
+        }
+
+        public async Task PushModelAsync(Page page)
+        {
+            await Application.Current.MainPage.Navigation.PushModalAsync(page);
+        }
     }
 }
