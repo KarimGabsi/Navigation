d49aac1b0fe9d1295d3efc13b7f86563b1c7870e
gabsi <karim.gabsi@vives.be>
2021-11-22 16:34:19 +0100

    Page service

---
+++ Navigation/Navigation/AppService/IPageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Navigation.AppService
+{
+    public interface IPageService
+    {
+        Task DisplayAlert(string title, string message, string cancel);
+        Task<bool> DisplayAlert(string title, string message, string accept, string cancel);
+        Task PushAync(Page page);
+        Task PushModelAsync(Page page);
+    }
+}
---
+++ Navigation/Navigation/AppService/PageService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Navigation.AppService
+{
+    public class PageService
+    {
+
+    }
+}
